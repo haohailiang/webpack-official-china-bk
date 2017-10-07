968adf5b701a065ac46156d1acfc0d9874b74881
haohailiang <646263409@qq.com>
2017-10-07 21:58:10 +0800

    抽取公共库,第三方库vendor

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
-var load = require('bundle-loader!./a.js');
-
-load(function(file) {
-  document.open();
-  document.write('<h1>' + file + '</h1>');
-  document.close();
-});
+var $ = require('jquery');
+$('h1').text('Hello World vendor');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './main.js',
+  entry: {
+    app: './main.js',
+    vendor: ['jquery'],
+  },
   output: {
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'vendor', /* filename= */'vendor.js')
+  ]
 };

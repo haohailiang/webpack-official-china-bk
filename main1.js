974acd89d98914e876b63538f9beb8444027a090
haohailiang <646263409@qq.com>
2017-10-07 15:28:35 +0800

    demo02多入口文件

--- main1.js
+++ main1.js
@@ -1,2 +1,1 @@
 document.write('<h1>Hello World</h1>');
-   
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  entry: './main.js',
-  output: {
-    filename: 'bundle.js'
+  entry: {
+    bundle1: './main1.js',
+    bundle2: './main2.js'
   },
+  output: {
+    filename: '[name].js'
+  }
 };

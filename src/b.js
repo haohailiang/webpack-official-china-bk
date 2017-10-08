889e085fdf6d1b258dee1f92a8112ad5eb94df78
haohailiang <646263409@qq.com>
2017-10-08 08:07:14 +0800

    页面的公用模块独立打包
    CommonsChunkPlugin

---
+++ src/b.js
@@ -0,0 +1,5 @@
+const libA = require('./lib/libA.js');
+const libB = require('./lib/libB.js');
+
+console.log(`文件B: ${libA}`);
+console.log(`文件B: ${libB}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry:[
-    './entry.js',
-  ],
+  entry: { 
+    a: "./src/a.js", 
+    b: "./src/b.js" 
+  },
   output: {
     path: __dirname + '/dist/',
-    publicPath: "http://www.baidu.com/dist/",
-    filename: 'result.js'
+    filename: '[name].bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: '配置文件修改名字'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common' // 指定公共 bundle 的名称。
     })
   ]
 };
